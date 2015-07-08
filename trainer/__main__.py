06689b618217733abff563f85f704a3038ba41b0
maximilianallan <maximilian.allan@gmail.com>
2015-07-08 14:07:34 +0100

    silly bug in commit

--- trainer/__main__.py
+++ trainer/__main__.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser(description='Train a random forest with little 
 parser.add_argument('-t', '--training-data', nargs='+', type=str, help='The list of training data files.', required=True)
 parser.add_argument('-m', '--masks', nargs='+', type=str, help='The list of mask files. Order should be the same as training data.', required=True)
 parser.add_argument('-n', '--num-labels', type=int, help='The number of labels we are using. This is for sanity checking against the number of labels found in mask files. Defaults to 2.', default=2)
-parser.add_argument('-v', '--video-file', type=str, help='Run the trained classifier on a video for testing. Leave blank to just test on the training data.')
+parser.add_argument('-v', '--video-file', type=str, help='Video file to test the trained classifier on. Leave blank to just test on the training data.')
 parser.add_argument('-s', '--save-file', type=str, help='File to save the output video. Leave blank to display in window.')
 
 
@@ -37,9 +37,9 @@ for f,g in zip(args.training_data, args.masks):
 t = Trainer(args.training_data, args.masks, "rf", args.num_labels)
 t.train()
 
-if args.video:
+if args.video_file:
 
-  cap = cv2.VideoCapture(args.video)
+  cap = cv2.VideoCapture(args.video_file)
   if args.save_file:
     writer = cv2.VideoWriter(args.save_file, cv2.cv.FOURCC(*"DIB "),
     int(cap.get(cv2.cv.CV_CAP_PROP_FPS)),
