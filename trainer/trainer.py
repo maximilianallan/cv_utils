73ea2e6c7c018e4b4436f738d264e02f63fdf501
Maximilian Allan <maximilian.allan@gmail.com>
2016-02-24 17:30:47 +0000

    updates to trainer

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -53,7 +53,7 @@ class RandomForest(Model):
     self.name = "RF"
     
   def setup_params(self):
-    self.params = dict(term_crit=(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER ,10,0.001) )
+    self.params = dict(term_crit=(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER ,4,0.001) )
   
 class BoostedClassifier(Model):
   def __init__(self):
@@ -173,12 +173,12 @@ class Trainer(object):
                 cs = ColorSpace(image=image)
                 hue = cs.get_hue()
                 sat = cs.get_sat()
+                o1 = cs.get_o1()
                 o2 = cs.get_o2()
-                o3 = cs.get_o3()
                 training_data[start_index:end_index,0] = hue.reshape((resolution,)).astype(np.float32, casting='safe')
                 training_data[start_index:end_index,1] = sat.reshape((resolution,)).astype(np.float32, casting='safe')
-                training_data[start_index:end_index,2] = o2.reshape((resolution,)).astype(np.float32, casting='safe')
-                training_data[start_index:end_index,3] = o3.reshape((resolution,)).astype(np.float32, casting='safe')
+                training_data[start_index:end_index,2] = o1.reshape((resolution,)).astype(np.float32, casting='safe')
+                training_data[start_index:end_index,3] = o2.reshape((resolution,)).astype(np.float32, casting='safe')
 
             if self.num_dims == 7:
                 red = image[:,:,0]
@@ -360,8 +360,8 @@ class Trainer(object):
         cs = ColorSpace(image=image)
         hue = cs.get_hue()
         sat = cs.get_sat()
+        o1 = cs.get_o1()
         o2 = cs.get_o2()
-        o3 = cs.get_o3()
 
         #filters = build_filters()
         #filters = np.asarray(filters)
@@ -389,8 +389,8 @@ class Trainer(object):
 
                     data[0] = float(hue[r,c])
                     data[1] = float(sat[r,c])
-                    data[2] = float(o2[r,c])
-                    data[3] = float(o3[r,c])
+                    data[2] = float(o1[r,c])
+                    data[3] = float(o2[r,c])
 
                 if self.num_dims == 7:
                     data[4] = float(red[r,c])
